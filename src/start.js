5d79dde1e1b2a7c3e5956ee51ae8de7f1a15012f
Robbie <akotoro22@gmail.com>
2019-09-16 09:29:32 +0000

    Added menu content to start button

---
+++ src/start.js
@@ -0,0 +1,24 @@
+dropdown = () => {
+  document.getElementById("myDropdown").classList.toggle("show");
+};
+openWord = () => {
+  testwindow = window.open(
+    "",
+    "Word",
+    "location=1,status=1,scrollbars=1,width=200,height=200"
+  );
+  testwindow.moveTo(0, 0);
+};
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
