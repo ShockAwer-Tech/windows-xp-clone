3631ac88449daa98285767184e8197ed6d3ca712
AdjoaEdwin <aedwin905@gmail.com>
2019-09-18 20:26:47 +0000

    Added preferences in settings

--- src/settings.js
+++ src/settings.js
@@ -1,17 +1,9 @@
+setdropdown = () => {
+  document.getElementById("setDropdown").classList.toggle("show");
+};
+
 minimize = () => {
   let minimize = document.getElementById("close");
   return (minimize.parentElement.parentElement.parentElement.style.display =
     "none");
 };
-// window.onclick = function(event) {
-//   if (!event.target.matches(".setdropdown")) {
-//     var dropdowns = document.getElementsByClassName("setdropdown-content");
-//     var i;
-//     for (i = 0; i < dropdowns.length; i++) {
-//       var openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains("show")) {
-//         openDropdown.classList.remove("show");
-//       }
-//     }
-//   }
-// };
