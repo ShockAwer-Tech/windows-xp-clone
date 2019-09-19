f72c2ead39f70f16af0ee623489356c1e551f9f0
AdjoaEdwin <aedwin905@gmail.com>
2019-09-19 09:45:51 +0000

    Added functionality to minimise, maximize and close the window for settings

--- src/settings.js
+++ src/settings.js
@@ -1,9 +1,52 @@
-setdropdown = () => {
-  document.getElementById("setDropdown").classList.toggle("show");
-};
+let settings = document.getElementById("settings");
+let close = document.getElementById("close");
+let x = document.getElementById("setDropdown");
+let max = document.getElementById("max");
+let elem = document.documentElement;
 
-minimize = () => {
-  let minimize = document.getElementById("close");
-  return (minimize.parentElement.parentElement.parentElement.style.display =
-    "none");
-};
+function openSettings() {
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+// opens the pop up window when settings is clicked
+settings.addEventListener("click", openSettings);
+
+// launches fullscreen
+max.addEventListener("click", function() {
+  let x = document.getElementById("setDropdown");
+  if (x.requestFullscreen) {
+    x.requestFullscreen();
+  } else if (x.mozRequestFullscreen) {
+    x.mozRequestFullScreen();
+  } else if (x.webkitRequestFullscreen) {
+    x.webkitRequestFullscreen();
+  } else if (x.msRequestFullscreen) {
+    x.msRequestFullscreen();
+  }
+});
+
+// close full screen
+max.addEventListener("click", function() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullscreen) {
+    document.mozCancelFullscreen();
+  } else if (document.webkitExitFullscreen()) {
+    document.webkitExitFullscreen();
+  }
+});
+
+// hides the window that displays when setting is clicked
+function closeForm() {
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+close.addEventListener("click", closeForm);
